eeb813a369aea385be45a904a6198b90ea5d7e38
sasaki_y <yusuke.sasaki.j7z@jpgr.denso.com>
2021-02-17 15:23:41 +0900

    F Documentモデルのフィールド拡充(相対パスはLRのExtensionFWとしても公開していないため削除)

--- src/ReviewFile/Models/IDocument.cs
+++ src/ReviewFile/Models/IDocument.cs
@@ -4,14 +4,44 @@ using System.Text;
 
 namespace LightningReview.ReviewFile.Models
 {
+    /// <summary>
+    /// ドキュメントのインタフェース
+    /// </summary>
     public interface IDocument
     {
+	    /// <summary>
+	    /// グローバルID
+	    /// </summary>
         string GID { get; set; }
+
+	    /// <summary>
+	    /// ローカルID
+	    /// </summary>
         string LID { get; set; }
+
+	    /// <summary>
+	    /// ドキュメント名
+	    /// </summary>
         string Name { get; set; }
 
+	    /// <summary>
+	    /// ドキュメントの絶対パス
+	    /// </summary>
+	    string AbsolutePath { get; set; }
+
+	    /// <summary>
+	    /// 関連づいているアプリケーション
+	    /// </summary>
+	    string ApplicationType { get; set; }
+
+	    /// <summary>
+	    /// このドキュメントに関連づく指摘の一覧
+	    /// </summary>
         //IEnumerable<IIssue> AllIssues { get; }
 
+        /// <summary>
+        /// このドキュメントに関連づくアウトラインの一覧
+        /// </summary>
         IEnumerable<IOutlineNode> OutlineNodes { get; }
     }
 }
